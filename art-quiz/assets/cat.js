54d26ddec33fb20aeadd5832eaa0520d15dfe62f
Antonina Berchuk <tonyaberch@gmail.com>
2021-11-05 17:10:36 +0100

    feat: add image

---
+++ art-quiz/assets/cat.js
@@ -0,0 +1,10 @@
+category: 'realism'
+category: 'impressionism'           
+category: 'renaissance'
+category: 'portrait'
+category: 'painting'
+category: 'landscape'
+category: 'avant-garde'               7
+category: 'surrealism'             5
+category: 'romanticism'             
+category: 'expressionism'          
